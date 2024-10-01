ebe061a41b7d01d796ce2c551cb9b9e59cdf735c
Paflias <paflias13@gmail.com>
2024-10-01 16:45:14 +0300

    'wip'

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -1,5 +1,8 @@
+import { InputBase$ChangeEvent } from "sap/m/InputBase";
 import BaseController from "./BaseController";
 import JSONModel from "sap/ui/model/json/JSONModel";
+import * as Nominatim from "nominatim-client";
+import MessageBox from "sap/m/MessageBox";
 
 type WeatherInfo = {
 	current_weather: {
@@ -7,6 +10,7 @@ type WeatherInfo = {
 		windspeed: number;
 		winddirection: number;
 	};
+	placeName: string;
 };
 
 /**
@@ -17,15 +21,50 @@ export default class Main extends BaseController {
 		const model = new JSONModel();
 		this.setModel(model);
 		void this.loadWeatherData();
+
+		// const input = this.byId("location");
+		// if (input.isA<Input>("sap.m.Input")) {   // type guard
+		// 	input.attachChange((evt) => {        // now TS knows tha is an Input
+		// 		const location = evt.getParameter("value");
+		// 	});
+		// }
 	}
 
-	async loadWeatherData(lat = "49.31", lon = "8.64") {
+	async loadWeatherData(lat = "49.31", lon = "8.64", placeName = "Walldorf") {
 		// default coordinates: Walldorf
 		const response = await fetch(
 			`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`
 		);
 		const jsonData = (await response.json()) as WeatherInfo;
-
+		jsonData.placeName = placeName;
 		(this.getModel() as JSONModel).setData(jsonData);
+	}
+
+	locationChange(evt: InputBase$ChangeEvent) {
+		const location = evt.getParameters().value;
+
+		Nominatim.createClient({
+			useragent: "UI5 TypeScript Tutorial App", // useragent and referrer required by the terms of use
+			referer: "https://localhost",
+		})
+			.search({ q: location })
+			.then((results) => {
+				if (results.length > 0) {
+					return this.loadWeatherData(
+						results[0].lat,
+						results[0].lon,
+						results[0].display_name
+					); // for simplicity just use the first/best match
+				} else {
+					MessageBox.alert(`Location ${location} not found`, {
+						actions: MessageBox.Action.CLOSE, // enums are now properties on the default export!
+					});
+				}
+			})
+			.catch(() => {
+				MessageBox.alert(`Failure while searching ${location}`, {
+					actions: MessageBox.Action.CLOSE, // enums are now properties on the default export!
+				});
+			});
 	}
 }
